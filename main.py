201baf94252311f03a0f27b76983ca62a6b8f6fc
Alexey Petukhov <putonstack@gmail.com>
2022-12-23 23:31:41 +0800

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import slack
+
+SLACK_TOKEN = os.environ.get("SLACK_TOKEN")
+
+def run():
+    if __name__ == "__main__":
+        main()
+
+def check_all_users_in_channel():
+    from slack_sdk import WebClient
+    from slack_sdk.errors import SlackApiError
+
+    client = WebClient
+
+
+def main():
+    check_all_users_in_channel()
